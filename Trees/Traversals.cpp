d7a09ee5cfc76bb5fe61e6e98e7a47f63c42f1b9
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-08-02 16:40:35 +0530

    Intro Tree.

---
+++ Trees/Traversals.cpp
@@ -0,0 +1,73 @@
+// #include <iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *newNode(int data)
+{
+    struct node *Node = new node;
+    Node->data = data;
+    Node->left = NULL;
+    Node->right = NULL;
+    return Node;
+}
+
+void preorder(struct node *Node)
+{
+    if (Node == NULL)
+    {
+        return;
+    }
+
+    cout << Node->data << "\t";
+    preorder(Node->left);
+    preorder(Node->right);
+}
+
+void postorder(struct node *Node)
+{
+    if (Node == NULL)
+    {
+        return;
+    }
+    postorder(Node->left);
+    postorder(Node->right);
+    cout << Node->data << '\t';
+}
+
+void inorder(struct node *Node)
+{
+    if (Node == NULL)
+        return;
+    inorder(Node->left);
+    cout << Node->data << "\t";
+    inorder(Node->right);
+}
+
+int main()
+{
+    struct node *root = newNode(1);
+
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+
+    cout << "PreOrder     ";
+    preorder(root);
+
+    cout << "\n"
+         << "POST Order   ";
+    postorder(root);
+
+    cout << "\n"
+         << "In Order     ";
+    inorder(root);
+
+    return 0;
+}
