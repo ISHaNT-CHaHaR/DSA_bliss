82154fb6d3278ff58ebba2e0f3ab044a5cf44dfd
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-08-05 23:48:25 +0530

    BST

---
+++ Trees/BSTinsert.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *left, *right;
+};
+
+struct node *newNode(int data)
+{
+    node *Node = new node;
+    Node->data = data;
+    Node->left = NULL;
+    Node->right = NULL;
+    return Node;
+}
+
+void NewNode(node *Node, int data)
+{
+
+    if (data > Node->data)
+    {
+        Node->right = newNode(data);
+    }
+    else
+    {
+        Node->left = newNode(data);
+    }
+}
+
+void inorder(node *Node)
+{
+    if (!Node)
+    {
+        return;
+    }
+
+    inorder(Node->left);
+    cout << Node->data << endl;
+    inorder(Node->right);
+}
+
+int main()
+{
+
+    struct node *Node = newNode(40);
+
+    NewNode(Node, 30);
+    NewNode(Node, 50);
+    NewNode(Node, 70);
+    //  NewNode(Node,20);
+    //  NewNode(Node,45);
+
+    inorder(Node);
+
+    return 0;
+}
