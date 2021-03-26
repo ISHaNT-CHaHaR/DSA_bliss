a612278453a02529572f97e8c5491bab07342ea8
chaharishant@gmail.com <chaharishant@gmail.com>
2021-03-26 16:24:16 +0530

    Start

---
+++ New.py
@@ -0,0 +1,25 @@
+# print('Hello! \nWhats poppin?')
+
+# if 6 > 5:
+#         print("6 is greater!")
+# if 6 < 5:
+#         print("Yeah! fool") # This is sweet comment in python
+
+# X = 5
+# Y = "Hey, You again!"
+# print(X, Y)
+
+# a = int(3)
+# b = str("Fushiguro Megumi!")
+# c = float(3)
+# A = "Best friend Toudou!"
+# print("\n", a,b, c)
+
+# print(type(X))
+
+# print(A)
+
+
+anime = ['Electron', 'Proton', 'Neutron']
+x, y, z = anime
+print(x + " " + y +" "+ z)
